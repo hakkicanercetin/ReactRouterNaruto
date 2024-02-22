826dcfe05a3c691709f39fdb486e12cfc32852d7
Hakkıcan ERÇETİN <hakkicanercetin@gmail.com>
2024-02-22 22:06:18 +0300

    Update

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -6,9 +6,9 @@ export function Home()
                 <h1 className="anim">What is Naruto?</h1>
                 <p className="anim">Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto. It tells  the story of Naruto Uzumaki,
                 a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village.</p>
-                <a href="/ReactRouterNaruto/characters" className="btn anim">Characters Page →</a>
+                <a href="./characters" className="btn anim">Characters Page →</a>
             </div>
-            <img src="../../../public/naruto.png" className="feature-img anim" />
+            <img src="../../public/naruto.png" className="feature-img anim" />
         </>
     )
 }
